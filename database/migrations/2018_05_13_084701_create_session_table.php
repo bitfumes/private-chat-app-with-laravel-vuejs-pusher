72b0f87e6b4bceedb106a930b904029a693831a9
Bitfumes <bitfumes@gmail.com>
2018-05-13 14:27:34 +0530

    schema

---
+++ database/migrations/2018_05_13_084701_create_session_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user1_id');
+            $table->unsignedInteger('user2_id');
+            $table->unique(['user1_id', 'user2_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('session');
+    }
+}
---
+++ database/migrations/2018_05_13_085415_create_chats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('message_id');
+            $table->unsignedInteger('session_id');
+            $table->unsignedInteger('user_id');
+            $table->boolean('type'); // 0 is for send and 1 is for receive
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
